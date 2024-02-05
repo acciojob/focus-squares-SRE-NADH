94fc380adb3b128ef1fef27ae964a9e1dc67c0e9
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2024-02-05 17:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let sq1 = document.getElementById("square1");
+let sq2 = document.getElementById("square2");
+let sq3 = document.getElementById("square3");
+
+let sqarray = document.getElementsByClassName("square");
+
+
+for(let i=0;i<3;i++){
+	sqarray[i].addEventListener("mouseover",onFocus);
+	sqarray[i].addEventListener("mouseout",onFocusOut);
+}
+
+
+function onFocus(e){
+for(let i=0;i<3;i++){
+	if(sqarray[i].id !== e.target.id){
+		sqarray[i].style.backgroundColor = "#6F4E37";
+	}
+}
+}
+
+	
+function onFocusOut(e){
+for(let i=0;i<3;i++){
+	sqarray[i].style.backgroundColor = "#E6E6FA"
+}
+}
